d7622861ae68719541efc61208ddd5e1450723fe
Amalie S <amaliesmidth@gmail.com>
2018-08-23 10:17:10 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,9 +1,16 @@
+#INSTANTIATE METHODS
+
 def instantiate_new_array
   @my_new_array = []
 end
 
-
 def array_with_two_elements
   @my_two_array = ['a', 'b']
+end
+
+#INDEXING METHODS
+
+def first_element(array)
+  array[0]
 end
 
