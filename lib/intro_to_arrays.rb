8c9a7ef99f213c1db35e99058b867f0dcaa69585
Amalie S <amaliesmidth@gmail.com>
2018-08-23 10:26:35 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+#INSTANTIATE METHODS
+
+def instantiate_new_array
+  @my_new_array = []
+end
+
+def array_with_two_elements
+  @my_two_array = ['a', 'b']
+end
+
+#INDEXING METHODS
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[array.length-1]
+end
+
+# METHODS TO RETURN VALUE FROM ARRAY
+
+def first_element_with_array_methods(array) 
+  first_element = array.shift
+end
+
+def last_element_with_array_methods(array)
+  last_element = array.pop
+end
+
+# METHODS TO GET INFO ABOUT ARRAY
+
+def length_of_array(array)
+  array.length
+end
+  
